f98cc6d24b2899dcb36b892aa22f5aeb213458a4
zain <zain.jahangir@eclipsegamingsystems.com>
2018-10-26 11:22:36 -0500

    Added project

---
+++ Assets/GamePlatformHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+
+class GamePlatformHandler : GamePlatform.Iface
+{
+    public static event Action StartSpin;
+
+    protected virtual void OnStartSpin()
+    {
+        var handler = StartSpin;
+        if (handler != null) handler();
+    }
+    public void SendMessageFromPlatform()
+    {
+        Debug.Log("Called from Platform: SendMessageFromPlatform");
+        OnStartSpin();
+    }
+}
---
+++ Assets/ThriftClient.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading;
+using Thrift.Protocol;
+using Thrift.Server;
+using Thrift.Transport;
+using UnityEngine;
+
+public class ThriftClient : MonoBehaviour
+{
+    private bool _connectionOpen;
+    private GameUi.Client _client;
+    public void Start()
+    {
+        GamePlatformHandler.StartSpin += GameUiHandler_OnStartSpin;
+
+        var thread = new Thread(StartServer);
+        thread.Start();
+    }
+
+    private void GameUiHandler_OnStartSpin()
+    {
+        Debug.Log("Event Invoked! Thanks to Platform :)");
+    }
+
+    private void StartServer()
+    {
+        // Create the handler and register a callback.
+        var handler = new GamePlatformHandler();
+        var processor = new GamePlatform.Processor(handler);
+        var serverTransport = new TServerSocket(9091);
+        var server = new TSimpleServer(processor, serverTransport);
+
+        Debug.Log("Starting Unity Server");
+        server.Serve();
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_connectionOpen) return;
+            Debug.Log("Connecting to Platform...");
+            var transport = new TSocket("localhost", 9090);
+            var protocol = new TBinaryProtocol(transport);
+            _client = new GameUi.Client(protocol);
+            transport.Open();
+            _connectionOpen = true;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (_client != null)
+            {
+                Debug.Log("Sending Message");
+                _client.SendMessageFromUI();
+            }
+        }
+    }
+}
